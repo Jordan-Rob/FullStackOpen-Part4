da3c42dd1f62c8ebbfbf579e721ebf116742a759
jordanrob709 <jordanrob709@gmail.com>
2021-01-01 20:34:01 +0300

    Exercise: 4.2 Blog list, step2

--- models/blog.js
+++ models/blog.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-      console.log('connected to MongoDB')
-  })
-  .catch(error => {
-      console.log('error connecting to MongoDB:', error.messge)
-  })
 
 const blogSchema = mongoose.Schema({
   title: String,
