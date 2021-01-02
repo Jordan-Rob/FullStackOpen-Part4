2816a17877d5c64cb1da927a486851eac1009b24
jordanrob709 <jordanrob709@gmail.com>
2021-01-02 22:13:11 +0300

    Exercise: 4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likess = blogs.map(b => b.likes)
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    return likess.reduce(reducer)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
