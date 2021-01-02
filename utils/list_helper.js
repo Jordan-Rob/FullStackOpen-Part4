21b8f57ac44fb69696567f9aa96ab7d392e41726
jordanrob709 <jordanrob709@gmail.com>
2021-01-02 22:30:13 +0300

    Exercise 4.5: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,18 @@ const totalLikes = (blogs) => {
     return likess.reduce(reducer)
 }
 
+const favoriteBlog = (blogs) => {
+    const likes = blogs.map(b => b.likes)
+    const top = likes.reduce( (a, b) => {
+        return Math.max(a,b)
+    })
+
+    const favorite = blogs.filter(b => b.likes === top)
+    return favorite[0]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
